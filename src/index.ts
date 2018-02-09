fb9d80c6063952f0b2bc8d6a3ca63e4cc05ddeef
Ben <ben@solero.me>
2018-02-09 13:55:04 +0000

    Fix linting

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
-import { app, BrowserWindow } from "electron"
-import * as path from  "path"
-import * as url from "url"
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
 
-var win:BrowserWindow
+let win: BrowserWindow;
 
-function createWindow () {
+function createWindow() {
     win = new BrowserWindow({
-        width: 500, height: 700,
-        resizable: false
-    })
+        height: 700, resizable: false,
+        width: 500,
+    });
 
-    win.setMenu(null)
+    win.setMenu(null);
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+    }));
 
-    win.on('closed', () => {
-        win = null
-    })
+    win.on("closed", () => {
+        win = null;
+    });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
     if (win === null) {
-        createWindow()
+        createWindow();
     }
-})
+});
